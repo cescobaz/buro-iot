65a8ecb652165a4bcdd9286187b0d1681e3cde38
cescobaz <francesco.burelli@protonmail.com>
2022-05-02 20:38:54 +0000

    mosquitto port is now 8883

--- mosquitto/mosquitto_docker_run.sh
+++ mosquitto/mosquitto_docker_run.sh
@@ -4,7 +4,7 @@ docker run -d \
   --restart always \
   --network burellixyz \
   --name mqtt \
-  -p 1883:1883 \
+  -p 8883:8883 \
   -v "$(pwd)/certs":'/mosquitto/certs' \
   -v "$(pwd)/mosquitto.conf":/mosquitto/config/mosquitto.conf \
   -v "$(pwd)/passfile":/mosquitto/config/passfile \
