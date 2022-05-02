29e344d540f3f654aa8c31ad783f7f35b018ca25
cescobaz <francesco.burelli@protonmail.com>
2022-05-02 20:45:48 +0000

    mosquitto_docker_run more reliable

--- mosquitto/mosquitto_docker_run.sh
+++ mosquitto/mosquitto_docker_run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+script_dir=$(dirname $(realpath $0))
+
 docker run -d \
   --restart always \
   --network burellixyz \
   --name mqtt \
   -p 8883:8883 \
-  -v "$(pwd)/certs":'/mosquitto/certs' \
-  -v "$(pwd)/mosquitto.conf":/mosquitto/config/mosquitto.conf \
-  -v "$(pwd)/passfile":/mosquitto/config/passfile \
+  -v "${script_dir}/certs":/mosquitto/certs \
+  -v "${script_dir}/mosquitto.conf":/mosquitto/config/mosquitto.conf \
+  -v "${script_dir}/passfile":/mosquitto/config/passfile \
   eclipse-mosquitto
