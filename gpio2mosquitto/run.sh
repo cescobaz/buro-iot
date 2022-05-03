c0755a9a5450ea5bfd7909e3ec5c4ce21fc7f34d
cescobaz <francesco.burelli@gmail.com>
2022-05-03 10:06:20 +0200

    gpio2mos run.sh as sudo

--- gpio2mosquitto/run.sh
+++ gpio2mosquitto/run.sh
@@ -4,7 +4,7 @@ set -e
 
 script_dir=$(dirname $(realpath $0))
 
-${script_dir}/gpio2mosquitto \
+sudo ${script_dir}/gpio2mosquitto \
 	--host 'burelli.xyz' --port 8883 \
 	--username "buro.$(hostname)" --password "$(cat ${script_dir}/mqtt_pass)" \
 	--gpio-input 27
