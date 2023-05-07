9d3ef4e039e81dab9a2476ca3cfc441868d4e1fb
Francesco Burelli <francesco.burelli@proton.me>
2023-05-07 14:48:11 +0200

    remove gpio2mosquitto and pig2log

--- gpio2mosquitto/install_service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-script_dir=$(dirname $(realpath $0))
-
-sudo cp "${script_dir}/gpio2mosquitto.service" /etc/systemd/system/gpio2mosquitto.service
--- pig2log/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-src_path=$(dirname $(realpath $0))
-
-gcc -o "$src_path/pig2log" "$src_path/pig2log.c" -pthread
