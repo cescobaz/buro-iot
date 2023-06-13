55ab622cfb1875360f79058dad30ab6feb4fd562
Francesco Burelli <francesco.burelli@proton.me>
2023-06-14 01:17:30 +0200

    fix mqtt-upload-image

--- camera/mqtt-upload-image.sh
+++ camera/mqtt-upload-image.sh
@@ -6,12 +6,13 @@ topic="$prefix/camera/$object_id/image"
 
 echo "INFO: taking picture and send to topic $topic ..."
 
-snapshot_command='libcamera-still'
-if ! type "$snapshot_command" > /dev/null; then
+if ! type 'libcamera-still' > /dev/null; then
   snapshot_command='raspistill'
 else
   snapshot_command='libcamera-still --immediate --flush 1 --rawfull'
 fi
+
+echo "INFO: using $snapshot_command"
 
 $snapshot_command --nopreview -o - |
   mosquitto_pub --id $mqtt_client_id --url "$mqtt_url/$topic" --stdin-file
