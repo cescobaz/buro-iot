5447fb30207de409ce2b5707a4aee21a4a3fa299
Francesco Burelli <francesco.burelli@proton.me>
2025-12-06 18:29:17 +0100

    update: node-red to 4.0.4 and home-assistant to 2025.12

--- camera/mqtt-camera.sh
+++ camera/mqtt-camera.sh
@@ -5,7 +5,7 @@ unique_id="$object_id"
 topic_config="homeassistant/camera/$object_id/config"
 topic="$prefix/camera/$object_id/image"
 
-payload="{\"topic\": \"$topic\",\"object_id\":\"$object_id\",\"unique_id\":\"$unique_id\",\"name\":\"$entity_name\"}"
+payload="{\"topic\": \"$topic\",\"default_entity_id\":\"camera.$object_id\",\"unique_id\":\"$unique_id\",\"name\":\"$entity_name\"}"
 
 echo "INFO:[$(date)] register camera at topic $topic_config with payload $payload"
 
